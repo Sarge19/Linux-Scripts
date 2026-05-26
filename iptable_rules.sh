4a17f441299e9143d3a7e722c480253e3bfae7e6
Sarge19 <isaiah.walry@gmail.com>
2026-05-26 13:19:50 -0400

    Iptables Script
    This script flushes the iptables rules then sets new riles and lists them

---
+++ iptable_rules.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "...Flushing Iptables rules"
+sudo iptables -F
+echo ""
+
+echo "...allwoing via client net"
+iptables -A INPUT -s 172.16.31.0/24 -p tcp --dport 49999 -j ACCEPT
+echo ""
+
+echo "...disabling via server net"
+iptables -A INPUT -s 172.16.30.0/24 -p tcp --dport 49999 -j REJECT
+echo""
+iptables -n -L --line-numbers | grep 1
